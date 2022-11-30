02a2cb74f199670fd52e1ab3a737fd58395e9ca1
kyle-109 <mrs0403@cau.ac.kr>
2022-12-01 01:48:41 +0900

    feat/interaction
    라이트로 비추면 감춰진 이미지가 나오도록 기능 추가.
    펜과 노트 잡기 추가
    NPC 트리거 위치 조정

---
+++ Assets/Scripts/LightSwitch.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSwitch : MonoBehaviour
+{
+    //빛 괘적을 그릴 라인 렌더러
+    //private LineRenderer LightLineRenderer;
+    RaycastHit hit;
+    public Transform rayTransform;
+    public float rayDistance;
+    bool triggerValue = false;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(triggerValue == true)
+        {
+            TurnOn();
+        }
+        else
+        {
+            TurnOff();
+        }
+        
+    }
+
+    public void TurnOn()
+    {
+        GameObject.Find("Switch").transform.GetChild(0).gameObject.SetActive(true);
+
+        if (Physics.Raycast(rayTransform.position, rayTransform.forward, out hit, rayDistance))
+        {
+            if (hit.collider.CompareTag("dyingMessageTrigger"))
+            {
+                GameObject.Find("Trigger").transform.GetChild(0).gameObject.SetActive(true);
+            }
+        }
+    }
+
+    public void TurnOff()
+    {
+        GameObject.Find("Switch").transform.GetChild(0).gameObject.SetActive(false);
+    }
+    public void setTrue()
+    {
+        triggerValue = true;
+    }
+
+    public void setFalse()
+    {
+        triggerValue = false;
+    }
+}
